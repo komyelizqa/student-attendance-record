2c7543e195613d45fcd21e990876b1da88d6f868
komyelizqa <yelizavkom@gmail.com>
2024-06-24 03:53:05 -0700

    change LLL condition

--- student_attendance_record.py
+++ student_attendance_record.py
@@ -14,23 +14,18 @@ class StudentAttendanceRecord:
         if any(char not in 'ALP' for char in s):
             raise InvalidStringException()
 
-        # initialize counters for absences and consecutive late days
+        # check condition with 3 or more days late
+        if "LLL" in s:
+            return False
+
+        # initialize counters for absences
         absence_count = 0
-        late_streak = 0
 
         # iterate through each character in the string
         for char in s:
             if char == 'A':
                 absence_count += 1
-            if char == 'L':
-                late_streak += 1
-                # return false if late count is more than 2
-                if late_streak > 2:
-                    return False
-            else:
-                # reset late streak if character is not 'L' - check consecutive late days
-                late_streak = 0
-            # return false if absence count is more than 1
+            # Return false if absence count is more than 1
             if absence_count > 1:
                 return False
         # return true if both conditions are satisfied
